b086a2b800321058e2d5bbe87b399f358d66a77b
unknown <DOKMANTASYON@HilalNarman.optimakstu.local>
2022-07-21 08:55:08 +0300

    21.7.22

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, Image,Text, View, FlatList, ScrollView, Dimensions } from 'react-native';
+import { StyleSheet, SafeAreaView, Image, Text, View, FlatList, ScrollView, Dimensions } from 'react-native';
 import news_data from './src/news_data.json';
 import NewsCard from './src/components/NewsCard';
 import news_banner_data from './src/news_banner_data.json';
@@ -8,25 +8,22 @@ function App() {
   const renderNews = ({ item }) => <NewsCard news={item} />;
   return (
     <SafeAreaView style={styles.container}>
-      <View >
-
-        <ScrollView >
-          {
-            news_banner_data.map(bannernews=><Image 
-              style={styles.banner_image}
-              source={{uri:bannernews.imageUrl}}/>)
-
-
-          }
-
-        </ScrollView>
-        <FlatList
-          keyExtractor={(item, index) => item.u_id.toString()}
-          data={news_data}
-          renderItem={renderNews}
-        />
-
-      </View>
+      <Text style={styles.headerText}>News</Text>
+      <FlatList
+        ListHeaderComponent={() => (
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {news_banner_data.map(bannerNews => (
+              <Image
+                style={styles.banner_image}
+                source={{uri: bannerNews.imageUrl}}
+              />
+            ))}
+          </ScrollView>
+        )}
+        keyExtractor={item => item.u_id.toString()}
+        data={news_data}
+        renderItem={renderNews}
+      />
     </SafeAreaView>
   );
 }
@@ -34,11 +31,17 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF9CA'
+    backgroundColor: '#eceff1',
+    marginTop:30,
   },
-  banner_image:{
-    height:Dimensions.get('window').height/5,
+  banner_image: {
+    height: Dimensions.get('window').height / 5,
+    width: Dimensions.get('window').width / 2,
   },
-})
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 50,
+  },
+});
 
 export default App;
